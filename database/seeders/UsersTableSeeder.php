161c59593b83ea2fa2514d84ee3dc5c229a36214
ChavadrexGP <111896058+ChavadrexGP@users.noreply.github.com>
2022-10-18 10:44:21 -0500

    18-10-22

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,8 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Suport\Facades\DB;
-use Illuminate\Suport\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Faker\Factory as Faker;
 
 class UsersTableSeeder extends Seeder
@@ -22,18 +22,17 @@ class UsersTableSeeder extends Seeder
             'email' => 'admin@algo.com',
             'username' => 'admin',
             'password' => Hash::make('123'),
-            'avatar' => 'https//thiscatdoesnotexist.com/',
+            'avatar' => 'https://thiscatdoesnotexist.com/',
             'role' => 'admin',
         ]);
-
         $faker = Faker::create();
-        for($i = 0; $i<10; $i++){
+        for($i=0; $i<10; $i++){
             DB::table('users')->insert([
                 'name' => $faker->name,
                 'email' => $faker->email,
                 'username' => $faker->userName,
                 'password' => Hash::make('111'),
-                'avatar' => 'https//thiscatdoesnotexist.com/',
+                'avatar' => 'https://thiscatdoesnotexist.com/',
                 'role' => 'user',
             ]);
         }
