99250e5b6bf41f297023c1857baa2cc6e5c8c303
ChavadrexGP <111896058+ChavadrexGP@users.noreply.github.com>
2022-10-28 21:28:48 -0500

    Post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UsersTableSeeder::class, 
-            TopicsTableSeeder::class
+            TopicsTableSeeder::class,
+            PostsTableSeeder::class
         ]);
     }
 }
